3580d857295ff54e0cc3f7cb2f863360247c6280
fmaxx <firsoffmaxim@gmail.com>
2016-09-10 23:29:25 +0500

    UIColor: from hex string extension

--- Swiftils/Classes/extensions/UIColor.swift
+++ Swiftils/Classes/extensions/UIColor.swift
@@ -11,31 +11,32 @@ import UIKit
 
 extension UIColor{
     convenience init(hex:String) {
-        var cString:String = hex.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet() as NSCharacterSet).uppercaseString
         
+        // trim empty spaces
+        var trimmed:String = hex.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet() as NSCharacterSet).uppercaseString
         
-        if (cString.hasPrefix("#")) {
-            cString = cString.substringFromIndex(cString.startIndex.advancedBy(1))
+        // find "#" symbol
+        if (trimmed.hasPrefix("#")) {
+            trimmed = trimmed.substringFromIndex(trimmed.startIndex.advancedBy(1))
         }
         
+        // base colors vars
         var red:CGFloat = CGFloat()
         var green:CGFloat = CGFloat()
         var blue:CGFloat = CGFloat()
         var alpha:CGFloat = CGFloat()
-//        red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat
         
-        if ((cString.characters.count) == 6) {
+        // extract colors from string
+        if ((trimmed.characters.count) >= 6) {
             var rgba:UInt32 = 0
-            NSScanner(string: cString).scanHexInt(&rgba)
-            
-            
+            NSScanner(string: trimmed).scanHexInt(&rgba)
             red =   CGFloat((rgba & 0xFF000000) >> 24) / 255.0
             green = CGFloat((rgba & 0x00FF0000) >> 16) / 255.0
             blue =  CGFloat((rgba & 0x0000FF00) >> 8) / 255.0
             alpha = CGFloat( rgba & 0x000000FF) / 255.0
-
         }
         
+        // result
         self.init(red: red, green:green, blue:blue, alpha:alpha)
     }
 }
